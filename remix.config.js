2782752aaec73a9e5d585102488592e34d24c85f
KunalKamboj1 <kunal.kamboj.62@gmail.com>
2025-05-09 16:27:01 +0530

    Fix: Update build commands to use npx

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,10 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverModuleFormat: "cjs",
   serverBuildTarget: "node-cjs",
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
   serverBuildPath: "build/index.js",
   assetsBuildDirectory: "public/build",
+  serverDependenciesToBundle: [
+    "@shopify/shopify-app-remix",
+    "@shopify/polaris"
+  ]
 }; 
